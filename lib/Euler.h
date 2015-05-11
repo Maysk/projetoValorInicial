935232903650eb226300767549f502d816a377c1
Matheus Mayron <matheusmayron@gmail.com>
2015-05-11 20:24:35 -0300

    ultimo commit de hoje.

--- lib/Euler.h
+++ lib/Euler.h
@@ -11,9 +11,6 @@ class Euler{
 	double t;
 	double dt;
 	int steps;
-	double multFacForward(double fnk,double fn1);
-	double multFacModify(double fnk,double fn1);
-	Result iteration(double error,double (*multFac) (double,double));
 public:
 	Euler(double (*dz) (double,double),double (*dw)(double,double,double,double,double),double (*dx)(double ,double ,double ,double ),double y1,double y2 ,double y3,double dy1,double dy2 ,double dy3,double t,double dt);
 	Result forward();
