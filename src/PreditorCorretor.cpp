a2b0e304d9b1d4e47ee84b35bb1549c009d0899d
Matheus Mayron <matheusmayron@gmail.com>
2015-06-05 21:02:35 -0300

    Preditor-corretor 4 ordem

--- src/PreditorCorretor.cpp
+++ src/PreditorCorretor.cpp
@@ -34,7 +34,8 @@ Result PreditorCorretor::pcTerceiraOrdem(int metodoUsado){
 	w[0] = dy2;
 	x[0] = dy3;
 
-    double resultado = 0;
+    double preditorZ,preditorW,preditorX,preditorY1,preditorY2,preditorY3;
+    double derivadaPreditorZ,derivadaPreditorW,derivadaPreditorX,derivadaPreditorY1,derivadaPreditorY2,derivadaPreditorY3;
 
     if(metodoUsado == FORWARD_EULER)
         {
@@ -82,8 +83,7 @@ Result PreditorCorretor::pcTerceiraOrdem(int metodoUsado){
         result.setDerivate(2,i,x[i]);
 	}
 
-    double preditorZ,preditorW,preditorX,preditorY1,preditorY2,preditorY3;
-    double derivadaPreditorZ,derivadaPreditorW,derivadaPreditorX,derivadaPreditorY1,derivadaPreditorY2,derivadaPreditorY3;
+    
 	for(int i = 3; i < result.getLength(); i++){
         preditorZ = z[i - 1] + dt * (23 * derivadaZ[i - 1] - 16 * derivadaZ[i - 2] + 5 * derivadaZ[i - 3]) / 12;
         preditorW = w[i - 1] + dt * (23 * derivadaW[i - 1] - 16 * derivadaW[i - 2] + 5 * derivadaW[i - 3]) / 12;
@@ -121,57 +121,108 @@ Result PreditorCorretor::pcTerceiraOrdem(int metodoUsado){
 	return result;
 
 }
+
 Result PreditorCorretor::pcQuartaOrdem(int metodoUsado){
 	Result aux;
 	Result result(steps+1);
 
-    if(metodoUsado ==  FORWARD_EULER)
-        {
-            Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = euler.forward();
-        }
+	Temporizador temporizador;
+    temporizador.iniciarTemporizador();
 
-    if(metodoUsado ==  BACKWARD_EULER)
-        {
-            Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = euler.backward(0.01);
-        }
+    double z[steps+1];
+	double w[steps+1];
+	double x[steps+1];
+	double derivadaZ[steps+1];
+	double derivadaW[steps+1];
+	double derivadaX[steps+1];
+	z[0] = dy1;
+	w[0] = dy2;
+	x[0] = dy3;
 
-    if(metodoUsado ==  EULER_MODIFY)
-        {
-            Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = euler.eulerModify(0.01);
-        }
+	double preditorZ,preditorW,preditorX,preditorY1,preditorY2,preditorY3;
+    double derivadaPreditorZ,derivadaPreditorW,derivadaPreditorX,derivadaPreditorY1,derivadaPreditorY2,derivadaPreditorY3;
 
-    if(metodoUsado ==  RUNGEKUTTA_2ORDEM)
-        {
-            RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = rungeKutta.rkSegundaOrdem();
-        }
+    if(metodoUsado ==  FORWARD_EULER){
+        Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = euler.forward();
+    }
 
-    if(metodoUsado ==  RUNGEKUTTA_3ORDEM)
-        {
-            RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = rungeKutta.rkTerceiraOrdem();
-        }
+    if(metodoUsado ==  BACKWARD_EULER){
+        Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = euler.backward(0.01);
+    }
 
-    if(metodoUsado ==  RUNGEKUTTA_4ORDEM)
-        {
-            RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
-            aux = rungeKutta.rkQuartaOrdem();
-        }
+    if(metodoUsado ==  EULER_MODIFY){
+        Euler euler(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = euler.eulerModify(0.01);
+    }
+
+    if(metodoUsado ==  RUNGEKUTTA_2ORDEM){
+        RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = rungeKutta.rkSegundaOrdem();
+    }
+
+    if(metodoUsado ==  RUNGEKUTTA_3ORDEM){
+        RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = rungeKutta.rkTerceiraOrdem();
+    }
+
+    if(metodoUsado ==  RUNGEKUTTA_4ORDEM){
+        RungeKutta rungeKutta(dz,dw,dx,0,0,0,0,0,0,(4*dt),dt);
+        aux = rungeKutta.rkQuartaOrdem();
+    }
 
 	for( int i = 0; i < aux.getLength(); i++){
 		result.setValue(0,i,aux.getValue(0,i));
 		result.setValue(1,i,aux.getValue(1,i));
 		result.setValue(2,i,aux.getValue(2,i));
+		z[i] = aux.getDerivate(0,i);
+		derivadaZ[i] = (*dz)(aux.getValue(0,i),aux.getValue(1,i));
+        w[i] = aux.getDerivate(1,i);
+        derivadaW[i] = (*dw)(aux.getValue(0,i),aux.getValue(1,i),aux.getValue(2,i),aux.getDerivate(1,i),aux.getValue(2,i));
+        x[i] = aux.getDerivate(2,i);
+        derivadaX[i] = (*dx)(aux.getValue(1,i),aux.getValue(2,i),aux.getDerivate(1,i),aux.getValue(2,i));
+        result.setDerivate(0,i,z[i]);
+        result.setDerivate(1,i,w[i]);
+        result.setDerivate(2,i,x[i]);
 	}
 
+    for(int i = 4; i < result.getLength(); i++){
+        preditorZ = z[i - 1] + dt * (55 * derivadaZ[i - 1] - 59 * derivadaZ[i - 2] + 37 * derivadaZ[i - 3] - 9 * derivadaZ[i - 4]) / 24;
+        preditorW = w[i - 1] + dt * (55 * derivadaW[i - 1] - 59 * derivadaW[i - 2] + 37 * derivadaW[i - 3] - 9 * derivadaW[i - 4]) / 24;
+        preditorX = x[i - 1] + dt * (55 * derivadaX[i - 1] - 59 * derivadaX[i - 2] + 37 * derivadaX[i - 3] - 9 * derivadaX[i - 4]) / 24;
 
+        preditorY1 = result.getValue(0, i - 1) + dt * (55 * z[i - 1] - 59 * z[i - 2] + 37 * z[i - 3] - 9 * z[i - 4]) / 24;
+        preditorY2 = result.getValue(1, i - 1) + dt * (55 * w[i - 1] - 59 * w[i - 2] + 37 * w[i - 3] - 9 * w[i - 4]) / 24;
+        preditorY3 = result.getValue(2, i - 1) + dt * (55 * x[i - 1] - 59 * x[i - 2] + 37 * x[i - 3] - 9 * x[i - 4]) / 24;
 
+        derivadaPreditorZ = (*dz)(preditorY1,preditorY2);
+        derivadaPreditorW = (*dw)(preditorY1, preditorY2, preditorY3, preditorW, preditorX);
+        derivadaPreditorX = (*dx)(preditorY2, preditorY3, preditorW, preditorX);
 
+        z[i] = z[i - 1] + dt * (9 * derivadaPreditorZ + 19 * derivadaZ[i - 1] - 5 * derivadaZ[i - 2] + derivadaZ[i - 3]) / 24;
+        w[i] = w[i - 1] + dt * (9 * derivadaPreditorW + 19 * derivadaW[i - 1] - 5 * derivadaW[i - 2] + derivadaW[i - 3]) / 24;
+        x[i] = x[i - 1] + dt * (9 * derivadaPreditorX + 19 * derivadaX[i - 1] - 5 * derivadaX[i - 2] + derivadaX[i - 3]) / 24;
 
+        derivadaPreditorY1 = z[i];
+        derivadaPreditorY2 = w[i];
+        derivadaPreditorY3 = x[i];
 
+        result.setValue(0, i, result.getValue(0,i - 1) + dt * (9 * derivadaPreditorY1 + 19 * z[i - 1] - 5 * z[i - 2] + z[i - 3]) / 24);
+        result.setValue(1, i, result.getValue(1,i - 1) + dt * (9 * derivadaPreditorY2 + 19 * w[i - 1] - 5 * w[i - 2] + w[i - 3]) / 24);
+        result.setValue(2, i, result.getValue(2,i - 1) + dt * (9 * derivadaPreditorY3 + 19 * x[i - 1] - 5 * x[i - 2] + x[i - 3]) / 24);
+
+        derivadaZ[i] = (*dz)(result.getValue(0,i), result.getValue(1,i));
+        derivadaW[i] = (*dw)(result.getValue(0,i), result.getValue(1,i), result.getValue(2,i), w[i], x[i]);
+        derivadaX[i] = (*dx)(result.getValue(1,i), result.getValue(2,i), w[i], x[i]);
+
+        result.setDerivate(0,i,z[i]);
+        result.setDerivate(1,i,w[i]);
+        result.setDerivate(2,i,x[i]);
+    }
+
+    result.setTime(temporizador.getTimeInSec());
+	return result;
 
 }
 
